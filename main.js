e965fb73ae8ee1f70a9953284bb4732bdad423a3
EliGarcia <garciaelisha93@yahoo.com>
2017-08-07 14:19:54 -0500

    put html is TL is main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1. First, find our UL Container
+let container = document.querySelector('.card');
+
+
+// 2. Create our Ajax Request
+let request = new XMLHttpRequest();
+request.addEventListener("load", displayProfile);
+request.open('GET', 'https://api.github.com/users/Galm1');
+request.send();
+
+// Our display function
+function displayProfile() {
+  // Parse our response text
+  let data = JSON.parse(this.responseText);
+  let myCard = '';
+
+  myCard += `
+
+  <header>
+    <h1>${data.login}</h1>
+  </header>
+
+  <div id="basics">
+    <h3>The Basics</h3>
+      <p>Name: ${data.name}</p>
+      <p>Github URL: ${data.html_url}</p>
+      <p>Email: ${data.email}</p>
+      <p>Company: ${data.company}</p>
+      <p>Website: ${data.html_url}</p>
+  </div>
+
+  <div id="theStory">
+    <h3>The Story</h3>
+      <p>${data.bio}</p>
+  </div>
+
+  <div ="profileImg">
+    <img src="${data.avatar_url}" alt="profile picture">
+  </div>
+
+`;
+
+
+  container.innerHTML = myCard;
+}
